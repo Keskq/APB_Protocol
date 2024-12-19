19e418fc16bb466e4053c49f5b24ab639dd2f0ca
koral reuben <149824651+Keskq@users.noreply.github.com>
2024-12-19 15:36:02 +0200

    Update Master.sv

--- Master.sv
+++ Master.sv
@@ -9,8 +9,7 @@ module apb_master(
       	output reg PENABLE,
       	output reg [7:0]PADDR,
       	output reg PWRITE,
-      	output reg [7:0]PWDATA,apb_read_data_out,
-      	output PSLVERR ); 
+      	output reg [7:0]PWDATA,apb_read_data_out); 
 
 
 reg [1:0] state, next_state;
@@ -32,33 +31,37 @@ always_comb begin
 		    next_state= IDLE;
 		else
 		    next_state= SETUP;
-		end
+	end
 	        
 	SETUP: begin
 	    PENABLE= 0;
-	    if(READ_WRITE) // if the action is writing
-		begin
+	    if(READ_WRITE) begin
 		    PADDR= apb_write_paddr;
 		    PWDATA= apb_write_data;
-		end
-	    else
-		begin
+		    PWRITE=1;
+	    end
+	    else begin
 		    PADDR= apb_read_paddr;
-		end
+		    PWRITE=0;
+	    end
+	    if (PADDR[8])
+                    PSEL2 = 1;                
+            else
+                    PSEL1 = 1;
+		
+	    next_state = ACCESS;	
+	end
+		
 	ACCESS: begin 
 	    if (PSEL1 || PSEL2) begin
-		      PENABLE = 1; 
-		      if (PSLVERR) begin
-		        next_state = IDLE; 
-		      end 
-		      else if (PREADY) begin
-			        if (READ_WRITE) begin
-			          apb_read_data_out = PRDATA;
-			        end
-				else begin
-				  PWDATA= apb_write_data;
-				end	
-			        next_state = (transfer) ? SETUP : IDLE; 
+		     PENABLE = 1; 
+		     if (PREADY) begin
+			  if (READ_WRITE) begin     
+			  end
+			  else begin
+			       apb_read_data_out = PRDATA;
+			  end	
+			  next_state = (transfer) ? SETUP : IDLE; 
 		      end 
 		      else begin
 		        next_state = ACCESS; 
@@ -66,46 +69,8 @@ always_comb begin
 	    end
 	    else begin
 		    next_state= IDLE;
-	    end	    
+	    end
+	end	
 end	
 			
 
-
-
-
-
-
-
-
-
-
-
-		
-  ACCESS: begin 
-      if (PSEL1 || PSEL2) begin
-        PENABLE= 1;
-        if (PSLVERR) begin
-          next_state= IDLE;
-        end 
-        else if (PREADY) begin
-          if (READ_WRITE) begin
-            apb_read_data_out= PRDATA; 
-          end
-          else begin
-            PWDATA= apb_write_data;
-          end
-          if (transfer) begin
-            next_state= SETUP; 
-          end 
-          else begin
-            next_state= IDLE;
-          end
-        end 
-        else begin
-          next_state= ACCESS; 
-        end
-      end 
-      else begin
-        next_state= IDLE;
-      end
-    end
